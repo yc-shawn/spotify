e72f59a435bccd69a21211ea1b20641356fb1ae3
Shawn Chen <ethanloatrchen@gmail.com>
2018-02-24 14:09:51 -0800

    routing issue

--- src/components/auth.js
+++ src/components/auth.js
@@ -11,22 +11,18 @@ import { setAuth, newAuth } from '../actions/auth.action';
 class Home extends Component {
   constructor(props){
     super(props);
-    this.state = {type: 'artist'}
-    let searchDebounce;
   }
   componentWillMount(){
-    let { setAuth, newAuth, location, history} = this.props;
-    // let stateKey = 'spotify_auth_state';
-    // let state = 'yc-shawn-spotify';
-    // sessionStorage.setItem(stateKey, state);
-    if (location.hash.length > 0){
-      newAuth(location.hash);
+    let { newAuth, match, history} = this.props;
+    let authString = match.params.authstring;
+    if (authString.includes('yc-shawn-spotify')){
+      newAuth(authString);
     }
     history.replace('');
   }
   render(){
     return (
-      <main>authing...</main>
+      <main style={{color: 'white'}}>authing...</main>
     )
   }
 }
@@ -35,4 +31,4 @@ function mapStateToProps(state){
   return { auth: state.auth }
 }
 
-export default connect(mapStateToProps, {setAuth, newAuth})(Home);
+export default connect(mapStateToProps, {newAuth})(Home);
